cabb9c2fdfc24282b821144267bdd7b44f50ac52
famfo <tf2005@gmx.de>
2021-09-13 19:50:21 +0200

    Licence things

--- src/commands.rs
+++ src/commands.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 pub mod ban;
 pub mod help;
 pub mod kick;
